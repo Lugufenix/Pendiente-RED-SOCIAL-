88cbf3ed311e5a073e8d1060a084371cb3dcc5ee
Luis Marquez <lugufenix@hotmail.com>
2017-05-18 17:14:31 -0500

    Add files via upload

---
+++ Documents/js/main.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+	
+$(document).ready(function(){
+      $('.slider').slider();
+    });
+ $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
