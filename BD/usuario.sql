a45a88ecbebe320328cc82d1f48a41eb3e6387f1
Luis Marquez <lugufenix@hotmail.com>
2017-05-18 16:49:23 -0500

    Add files via upload

---
+++ BD/usuario.sql
@@ -0,0 +1,10 @@
+CREATE TABLE usuario(
+	id_usuario int AUTO_INCREMENT PRIMARY KEY,
+	nombre_usuario varchar(10),
+	password char(8)
+);
+
+INSERT INTO usuario(nombre_usuario,password) VALUES("mcmanus","kc5Ld@rT");
+INSERT INTO usuario(nombre_usuario,password) VALUES("elpelucas","PelOTE5$");
+INSERT INTO usuario(nombre_usuario,password) VALUES("elminimau","s0yM@urr");
+INSERT INTO usuario(nombre_usuario,password) VALUES("lucaloco","9am3rTU%");
